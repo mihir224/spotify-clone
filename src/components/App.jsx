7d1bdced72b0eb1077c68e81fca44d293a1e290c
mihir224 <mihirsaini25@gmail.com>
2022-11-13 12:54:43 +0530

    minor changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import Header from "./Header";
+import PlaylistRect from "./PlaylistRect";
 import Footer from "./Footer";
+import playlists from "../playlistsRect";
 
+
+function createPlayList(playlist){
+    return(
+        <PlaylistRect key={playlist.id} id={playlist.id} img={playlist.imgURL} name={playlist.name} />
+    );
+}
 function App(){
     return (
         <div>
-        <div>
-            <Header />
-        </div>
-        <div>
+            <Header />    
+            {playlists.map(createPlayList)}   
             <Footer />
-        </div>
         </div>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,32 @@ import Logo from "../images/img.png";
 import "../Header.css";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+const TimeOfDay=new Date().getHours();
+    function greetingText(){
+        if(TimeOfDay>=0&&TimeOfDay<12){
+            return "morning";
+        }
+        if(TimeOfDay>=12&&TimeOfDay<16){
+            return "afternoon";
+        }
+        if(TimeOfDay>=16&&TimeOfDay<23){
+            return "evening";
+        }
+    }
 
  function Header(){
      const username="Mihir Saini";
      return(
-         <div>
+        <div>
+        <div className="headerDiv">
          <form>
          <button className="arrows"><span><ArrowBackIosIcon /></span></button> 
          <button className="arrows"><span><ArrowForwardIosIcon /></span></button>
-         <button className="profile"><img src={Logo} height="35" width="35"></img><span >{username}</span></button>
+         <button className="profile"><img src={Logo} height="40" width="40" alt="user-img"></img><span >{username}</span></button>
          </form>
          </div> 
+         <div className="greetingDiv"><h1>Good {greetingText()}</h1></div>
+         </div>
      );
  }
 
