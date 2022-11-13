fc2147c06f890e63c72db675f9da11823e44dc9d
mihir224 <mihirsaini25@gmail.com>
2022-11-13 17:35:22 +0530

    array for sq playlists

---
+++ src/playlistsSq.js
@@ -0,0 +1,35 @@
+const playlistsSq=[
+    {
+    id:1,
+    imgURL:"https://uploads-ssl.webflow.com/5e36e6f21212670638c0d63c/5e39d85cee05be53d238681a_likedSongs.png",
+    name:"Liked Songs",
+    artists:"Snoop Dogg, Charles Hamilton, Pritam and more"
+},
+{
+    id:2,
+    imgURL:"https://uploads-ssl.webflow.com/5e36e6f21212670638c0d63c/5e39d85cee05be53d238681a_likedSongs.png",
+    name:"Liked Songs",
+    artists:"Snoop Dogg, Charles Hamilton, Pritam and more"
+},
+{
+    id:3,
+    imgURL:"https://uploads-ssl.webflow.com/5e36e6f21212670638c0d63c/5e39d85cee05be53d238681a_likedSongs.png",
+    name:"Liked Songs",
+    artists:"Snoop Dogg, Charles Hamilton, Pritam and more"
+},
+{
+    id:4,
+    imgURL:"https://uploads-ssl.webflow.com/5e36e6f21212670638c0d63c/5e39d85cee05be53d238681a_likedSongs.png",
+    name:"Liked Songs",
+    artists:"Snoop Dogg, Charles Hamilton, Pritam and more"
+},
+{
+    id:5,
+    imgURL:"https://uploads-ssl.webflow.com/5e36e6f21212670638c0d63c/5e39d85cee05be53d238681a_likedSongs.png",
+    name:"Liked Songs",
+    artists:"Snoop Dogg, Charles Hamilton, Pritam and more"
+}
+
+];
+
+export default playlistsSq;
