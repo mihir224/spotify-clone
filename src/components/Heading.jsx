6d5db3c8915d4023899f82aba9f709b73fecf839
mihir224 <mihirsaini25@gmail.com>
2022-11-15 13:08:54 +0530

    separate component for heading

---
+++ src/components/Heading.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../Heading.css";
+const TimeOfDay=new Date().getHours();
+    function greetingText(){
+        if(TimeOfDay>=0&&TimeOfDay<12){
+            return "morning";
+        }
+        if(TimeOfDay>=12&&TimeOfDay<16){
+            return "afternoon";
+        }
+        if(TimeOfDay>=16&&TimeOfDay<24){
+            return "evening";
+        }
+    }
+
+function Heading(){
+    return (
+    <div className="greetingDiv"><h1>Good {greetingText()}</h1></div>
+    );
+}
+
+export default Heading;
