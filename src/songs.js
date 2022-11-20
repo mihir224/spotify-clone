2716c6cc113174d9bfab6294eb04b4975e215209
mihir224 <mihirsaini25@gmail.com>
2022-11-21 00:40:18 +0530

    added music files

---
+++ src/songs.js
@@ -0,0 +1,20 @@
+import NoLove from "./music/No Love.mp3";
+import Kesariya from "./music/Kesariya.mp3"
+const songs=[
+    {
+        id:1,
+        src:NoLove,
+        img:"https://i1.sndcdn.com/artworks-GDWvDYFQsThZ8eCf-TbqvVw-t500x500.jpg",
+        name:"No Love",
+        artist:"Shubh"
+    },
+    {
+        id:2,
+        src:Kesariya,
+        img:"https://feeds.abplive.com/onecms/images/uploaded-images/2022/07/17/23a74332dc133d8e7be1b77caf5925b31658040625_original.jpg",
+        name:"Kesariya",
+        artist:"Pritam"
+    }
+];
+
+export default songs;
